c57981985d473abcb83a95a557f1af92784ba6bf
Alex Jiang <azhewangyang@hotmail.com>
2016-09-06 11:48:36 -0700

    Add fields "neighbors" for GraphNode.java

--- src/GraphNode.java
+++ src/GraphNode.java
@@ -1,10 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Mingzhe.Jiang on 9/6/2016.
  */
 public class GraphNode {
     // This is the GraphNode definition
     public int key;
+    public List<GraphNode> neighbors;
     public GraphNode(int k) {
         key = k;
+        neighbors = new LinkedList<GraphNode>();
     }
 }

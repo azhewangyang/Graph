1418ef66e1773555af91f34c0f0aa63856ca4ddc
Alex Jiang <azhewangyang@hotmail.com>
2016-09-06 11:44:05 -0700

    Newly added class is GraphNode.java

---
+++ src/GraphNode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Mingzhe.Jiang on 9/6/2016.
+ */
+public class GraphNode {
+    // This is the GraphNode definition
+    public int key;
+    public GraphNode(int k) {
+        key = k;
+    }
+}

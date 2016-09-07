826808752f6d4be81b7c6a151de194036beac550
Alex Jiang <azhewangyang@hotmail.com>
2016-09-06 23:01:06 -0700

    Clear typos in BreadthFirstPaths, DepthFirstPath, DepthFirstSearch. Add GraphMethod class. Test GraphMethod in main

--- src/GraphMain.java
+++ src/GraphMain.java
@@ -1,10 +1,16 @@
+import java.util.List;
+
 /**
  * Created by Mingzhe on 5/8/2016.
  */
 public class GraphMain {
     public static void main(String[] args) {
-        Graph g = new Graph(9);
-        System.out.println(g.toString());
-        System.out.println("WTF!");
+        //Graph g = new Graph(9);
+        //System.out.println(g.toString());
+        //System.out.println("WTF!");
+        List<GraphNode> graph = GraphMethod.createGraph();
+        GraphMethod.print(graph);
+        GraphMethod.BFSPrint(graph);
+        GraphMethod.DFSPrint(graph);
     }
 }
